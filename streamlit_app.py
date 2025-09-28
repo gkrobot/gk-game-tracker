777ea3c43c73afdf40d02a6b9f6afdad0846776b
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-09-28 11:59:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Welcome to gameTrax")
 
-selected_game = st.selectbox(label="Select your game", options=["Phase 10", "Skip-Bo", "Uno"])
+selected_game = st.selectbox(label="Select your game", options=[None, "Phase 10", "Skip-Bo", "Uno"], default=None)
 
 if selected_game:
   st.info(f"You're playing {selected_game}!")
