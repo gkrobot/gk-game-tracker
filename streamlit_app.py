1d379e1e746f41d7830f83c95d4a06449f8ae1b9
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-09-28 09:00:28 -0500

    Correcting syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Welcome to gameTrax")
 
 st.write("Select your game")
-st.selectbox(["Phase 10", "Skip-Bo", "Uno"])
+st.selectbox(options=["Phase 10", "Skip-Bo", "Uno"])
