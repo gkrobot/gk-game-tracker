6c9aeac342390cc474d14400e2dbb0912ea5a402
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-09-28 08:59:43 -0500

    Updated header and added select box

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Welcome to gameTrax")
+
+st.write("Select your game")
+st.selectbox(["Phase 10", "Skip-Bo", "Uno"])
