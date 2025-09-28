78d674bc87005abaaeeef90968d5b29e7d851357
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-09-28 11:54:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("Welcome to gameTrax")
 
 selected_game = st.selectbox(label="Select your game", options=["Phase 10", "Skip-Bo", "Uno"])
 
-if selected_game = 'Skip-Bo':
+if selected_game == 'Skip-Bo':
   st.image("https://www.pngfind.com/pngs/m/227-2273272_skip-bo-skip-bo-clipart-hd-png-download.png")
