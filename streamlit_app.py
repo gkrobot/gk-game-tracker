3db5874dc42ddccb439ed7a899dec590ea1adb01
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-10-01 21:21:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ authenticator = Authenticator()
 st.title("Welcome to gameTrax")
 
 if 'authenticated' not in st.session_state or st.session_state.authenticated == False:
-    st.write('Not authenticated')
     st.session_state.authenticated = False
     authenticator.login()
 
