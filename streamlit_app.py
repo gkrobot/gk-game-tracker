87d938cca712f1377888a32571b18e9fb1de355e
gkrobot <115166857+gkrobot@users.noreply.github.com>
2025-10-30 02:20:39 +0000

    Fixing update

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,11 @@ if 'authenticated' in st.session_state and st.session_state.authenticated == Tru
                 st.session_state.data = pd.concat([st.session_state.data, new_df], ignore_index=True)
 
                 # Update the Google Sheet with the DataFrame's new data
-                st.session_state.google_conn.update(st.session_state.data)
+                st.session_state.google_conn.update(
+                    data=st.session_state.data,
+                    worksheet="Active"
+                )
 
                 st.success("New game record added successfully!")
-                time.sleep(1)
+                time.sleep(4)
                 st.rerun()
